8a94d8b43b8de24e27f3e46c95178c9ba3e20756
axard-midday <axard.midday@gmail.com>
2013-08-22 09:27:57 +0400

    Добавил заголовок всей библиотеки. Фуух... наконец-то. Почему-то отказывался добавляться пока не поменял местами строки

--- libs/libaskue/src/libaskue.h
+++ libs/libaskue/src/libaskue.h
@@ -1,14 +1,13 @@
 #ifndef LIBASKUE_H_
 #define LIBASKUE_H_
 
-#include <libaskue/script_argument.h>
-#include <libaskue/script_stream.h>
-#include <libaskue/script_enviroment.h>
-#include <libaskue/sprintf_datetime.h>
+#include <libaskue/time_fu.h>
+#include <libaskue/script_arg.h>
 #include <libaskue/uint8_array.h>
 #include <libaskue/write_msg.h>
 #include <libaskue/checksum.h>
 #include <libaskue/my.h>
+#include <libaskue/journal.h>
 #include <libaskue/macro.h>
 #include <libaskue/rs232.h>
 #include <libaskue/bcd.h>
